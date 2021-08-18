8b7feddf0228a1262295a04c472da952ef539242
Marc Majcher <majcher@gmail.com>
2021-08-18 17:09:08 -0500

    lesson

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ const API = 'http://localhost:3001/sushis';
 function App() {
   const [sushis, setSushis] = useState([]);
   const [beltPosition, setBeltPosition] = useState(0);
+  const [money, setMoney] = useState(100);
   const DISPLAY_COUNT = 4;
 
   useEffect(() => {
@@ -15,17 +16,25 @@ function App() {
       .then((json) => setSushis(json));
   }, []);
 
-  function setEaten(id) {
-    setSushis(
-      sushis.map((sushi) =>
-        sushi.id === id ? { ...sushi, eaten: true } : sushi
-      )
-    );
+  function setEaten(piece) {
+    const remainingMoney = money - piece.price;
+
+    if (!piece.eaten && remainingMoney >= 0) {
+      setMoney(remainingMoney);
+
+      setSushis(
+        sushis.map((sushi) =>
+          sushi.id === piece.id ? { ...sushi, eaten: true } : sushi
+        )
+      );
+    }
   }
 
   function advanceBelt() {
     setBeltPosition((beltPosition + DISPLAY_COUNT) % sushis.length);
   }
+
+  const eatenSushi = sushis.filter((sushi) => sushi.eaten);
 
   return (
     <div className="app">
@@ -34,7 +43,7 @@ function App() {
         handleMoreClick={advanceBelt}
         handleEatSushi={setEaten}
       />
-      <Table />
+      <Table money={money} plates={eatenSushi} />
     </div>
   );
 }
--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function Sushi({ sushi, handleEatSushi }) {
-  const { id, name, img_url, price, eaten } = sushi;
+  const { name, img_url, price, eaten } = sushi;
 
   return (
     <div className="sushi">
-      <div className="plate" onClick={() => handleEatSushi(id)}>
-        {eaten ? null : <img src={img_url} alt={name} width="100%" />}
+      <div className="plate" onClick={() => handleEatSushi(sushi)}>
+        {eaten ? '' : <img src={img_url} alt={name} width="100%" />}
       </div>
       <h4 className="sushi-details">
         {name} - ${price}
